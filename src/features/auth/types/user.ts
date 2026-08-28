968abd2e8db157037218f451dd569abb98398c96
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-08-28 16:03:36 +0300

    chore: add socket.io-client, scrollbar styles, user type updates

--- src/features/auth/types/user.ts
+++ src/features/auth/types/user.ts
@@ -1,23 +1,29 @@
-export interface User {
+﻿export interface User {
   id: string;
   firstName: string;
   lastName: string;
   email: string;
-  headline?: string;
-  bio?: string;
-  company?: string;
+  headline?: string | null;
+  bio?: string | null;
+  company?: string | null;
   avatar?: {
     publicId: string;
-    secure_url: string;
-  };
+    secure_url?: string;
+    url?: string;
+    resourceType?: string;
+  } | null;
   coverPhoto?: {
     publicId: string;
-    secure_url: string;
-  };
+    secure_url?: string;
+    url?: string;
+    resourceType?: string;
+  } | null;
   resume?: {
     publicId: string;
-    secure_url: string;
-  };
+    secure_url?: string;
+    url?: string;
+    resourceType?: string;
+  } | null;
   createdAt: string;
   updatedAt: string;
 }
